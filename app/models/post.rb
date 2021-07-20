4caf1365b1bde1838cd76d33ed72b6c9c8312d69
Joyce Lee <joycelee901@gmail.com>
2021-07-20 12:11:11 -0500

    commit tree 651525c380c530f81d509dfd75e24874f613a7bd

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+
+    def clickbait
+        # click_titles = ["Won't Believe", "Secret", "Top", "Guess"]
+        # unless title.include?(})
+        errors.add(:title, "Title not clickbait-y enough") unless 
+    title.present? && title.include?("Won't Believe" || "Secret" || "Top" || "Guess")        
+    end
 end
